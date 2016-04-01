a1547d940812e2926e82a627fb9eb34cff036a4c
Blake Murdock <blakemurdo@gmail.com>
2016-04-01 14:09:18 -0400

    Splash Screen placeholder and logic created

---
+++ app/src/main/java/ca/blake/georgian/dollify/dollify/SplashScreen.java
@@ -0,0 +1,36 @@
+package ca.blake.georgian.dollify.dollify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent openMainMenu = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(openMainMenu);
+
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
+    }
+
+}
