022083546bdd3ccaf7e2a04eb2c514bc2e2e4d76
Blake Murdock <blakemurdo@gmail.com>
2016-04-18 18:01:30 -0400

    Added new images, changed UI

--- app/src/main/java/ca/blake/georgian/dollify/dollify/FinishActivity.java
+++ app/src/main/java/ca/blake/georgian/dollify/dollify/FinishActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
